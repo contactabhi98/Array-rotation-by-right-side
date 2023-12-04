8e18c27dc98a9de85b3162c6ff2b0e140c030ad3
contactabhi98 <75126257+contactabhi98@users.noreply.github.com>
2023-12-04 18:54:34 +0530

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,22 @@
+function rotateArray(arr) {
+    console.log("Original Array: ", arr);
+    let shift_count = 2;
+    let temp;
+
+    while (shift_count) {
+        let temp = arr[arr.length - 1];
+        for (let i = arr.length - 1; i >= 1; i--) {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[0] = temp;
+        shift_count--;
+    }
+
+    return arr;
+}
+
+// Example usage
+const myArray = [1, 2, 3, 4, 5];
+const rotatedArray = rotateArray(myArray);
+console.log("Rotated Array in a right side direction by 2:", rotatedArray);
